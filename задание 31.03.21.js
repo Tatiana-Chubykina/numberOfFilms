29b2668b8f0c8d9aa99ef11f68f0b775bb37607e
Tatiana-Chubykina <81134626+Tatiana-Chubykina@users.noreply.github.com>
2021-05-24 19:52:04 +0700

    Add files via upload

---
+++ задание 31.03.21.js
@@ -0,0 +1,36 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+фильмы: {
+    "логан: 87"
+};
+if(numberOfFilms)
+{
+    console.log(numberOfFilms)
+};
+const personalMovieDB = {
+    const: numberOfFilms,
+    фильмы: {},
+    действующиелица : {},
+    жанры: [],
+    частноелицо : ложь
+};
+if(personalMovieDB)
+{
+    console.log(personalMovieDB);
+};
+const a = prompt("Один из последних просмотренных фильмов?")
+фильмы: {
+    'logan: Сумерки'
+};
+b = prompt("На сколько оцените его?"), 
+c = prompt("Один из последних просмотренных фильмов?"),
+d = prompt("На сколько оцените его?");
+фильмы: {
+    'logan: 10 баллов'
+};
+
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
+if(personalMovieDB)
+{
+    console.log(personalMovieDB);
+};
